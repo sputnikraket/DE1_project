c1fb42ac36e45acd6ff8986967ee15bf6a065aa9
4rhi <aram.himelius@gmail.com>
2024-03-13 10:25:03 +0100

    updated replicator script and added Data_to_replicate.csv

--- data_replicator.py
+++ data_replicator.py
@@ -1,16 +1,14 @@
 import pandas as pd
-replicFactor = 25
-# Replace 'input_csv_path' with the path to your input CSV file
-input_csv_path = "replicated_Data_x100.csv"
+replicFactor = 11600 #makes it 4GB
 
-# Read the CSV file into a Pandas DataFrame
+input_csv_path = "Data_to_replicate.csv"
+
+#csv to pandas
 original_df = pd.read_csv(input_csv_path)
 
-# Replicate each row 100 times
 replicated_df = pd.concat([original_df] * replicFactor, ignore_index=True)
 
-# Replace 'output_csv_path' with the desired path for the output CSV file
-output_csv_path = f"replicated_Data_x{str(replicFactor*100)}.csv"
+output_csv_path = "replicated_Data_4GB.csv"
 
 # Write the replicated DataFrame to a new CSV file
 replicated_df.to_csv(output_csv_path, index=False)
